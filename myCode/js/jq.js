68c74ac77dd501216f26829d401ff86456d1eee2
Alexey <laiten808@gmail.com>
2017-08-17 16:11:54 +0400

    -

--- myCode/js/jq.js
+++ myCode/js/jq.js
@@ -4,3 +4,16 @@ $('.myclass').each(function(index){ /*code*/ });
 var val = $(this).attr('value');
 //всем дивкам класса .info установить серый цвет
 $('div.info').css('background','grey');
+//удаление аттрибута
+$('.myClass').removeAttr('nameAttr');
+
+
+
+
+$('.dial').attr('style', 'background: red !important');
+$('.dial').style('color', 'red', 'important');
+
+
+addImportantStyle('.element', 'border: 1px solid #000; height: 50px !important;')
+
+
