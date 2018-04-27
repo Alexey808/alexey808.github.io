431b77f8fc8e3bb9bf74fb0a8a1b7551d29da46b
Alexey <laiten808@gmail.com>
2018-04-27 22:56:42 +0400

    -

---
+++ myCode/angular/noties.js
@@ -0,0 +1,57 @@
+//--- $postLink -------------------------------------------
+function myDirective() {
+  restrict: 'E',
+  scope: { foo: '=' },
+  compile: function compile($element, $attrs) {
+    return {
+      pre: function preLink($scope, $element, $attrs) {
+        //все элементы ещё НЕ связанны
+      },
+      post: function postLink($scope, $element, $attrs) {
+        //все элементы связанны и готовы к работе
+      }
+    };
+  }
+}
+
+//старый варинат
+function myDirective() {
+  restrict: 'E',
+  scope: { foo: '=' },
+  link: function postLink($scope, $element, $attrs) {
+    //все элементы связанны и готовы к работе
+  }
+}
+//новый вариант
+var myComponent = {
+  ...
+  controller: function () {
+    this.$postLink = function () {
+      //все элементы связанны и готовы к работе
+    };
+  }
+};
+
+//--- $onChanges ------------------------------------------
+!$onChanges вызывается в локальном контроллере компонентов 
+из изменений, которые произошли в родительском контроллере. 
+Изменения, которые происходят от родителя, которые вводятся 
+в используемый компонент через bindings: {}.
+
+
+var childComponent = {
+  bindings: { user: '<' },
+  controller: function () {
+    this.$onChanges = function (changes) {
+        // `changes` - это специальный экземпляр объекта-конструктора,
+        // содержит хэш объекта изменения и
+        // также содержит функцию, называемую `isFirstChange ()`
+        // он реализован в исходном коде с использованием объекта-конструктора
+        // и прототип метода для создания функции `isFirstChange ()`
+    };
+  }
+};
+
+angular
+  .module('app')
+  .component('childComponent', childComponent);
